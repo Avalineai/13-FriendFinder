1caa471a05fa3e331b91f9db01a98f29b4f889f5
Calypso Blue <46878997+Avalineai@users.noreply.github.com>
2019-05-21 20:27:56 -0700

    jquery added to retrieve data from user input

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -2,7 +2,12 @@ var path = require("path");
 
 module.exports = function(app) {
 
-// app.get("/friends", function(req, res){
-//     res.sendFile(path.join(__dirname, "../public/"))
-// })
+app.get("/survey", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/survey.html"))
+})
+
+
+app.get("/home", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/home.html"))
+})
 }
