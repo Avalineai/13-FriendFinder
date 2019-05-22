ffb3cbe58835c0f893325202402525f2c4d0b9cd
Calypso Blue <46878997+Avalineai@users.noreply.github.com>
2019-05-21 19:48:54 -0700

    some working routes established

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friendArray = [
+    {
+        "name": "Jesse",
+        "photo": "https://imagelink",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "James",
+        "photo": "https://imagelink",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Pikachu",
+        "photo": "https://imagelink",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+]
+
+module.exports = friendArray
